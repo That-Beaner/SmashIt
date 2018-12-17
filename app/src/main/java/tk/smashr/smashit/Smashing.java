962338de1cc4d08f7ff7e2cefc38d631336f4ee5
ZeevoX <langer.timothy@gmail.com>
2018-12-17 13:33:57 +0100

    Use AppCompat AlertDialog class instead
    * Migrate App to AppCompat

--- app/src/main/java/tk/smashr/smashit/Smashing.java
+++ app/src/main/java/tk/smashr/smashit/Smashing.java
@@ -1,11 +1,11 @@
 package tk.smashr.smashit;
 
-import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
